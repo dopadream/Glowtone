11e42eee6b25f7e2f3705943ac721c3849a3eb7d
dopadream <slyshidiscord@gmail.com>
2026-07-05 06:25:12 -0500

    Revert "remove AO from bright lights"
    This reverts commit c7b9c140c87a6a10ef7410de831a9f07229bfe8d.

--- src/main/java/net/frozenblock/glowtone/mixin/client/SimpleModelWrapperMixin.java
+++ src/main/java/net/frozenblock/glowtone/mixin/client/SimpleModelWrapperMixin.java
@@ -6,26 +6,16 @@ import java.util.List;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.frozenblock.glowtone.GlowtoneConstants;
-import net.frozenblock.glowtone.client.render.model.GlowtoneFullbrightModel;
 import net.minecraft.client.resources.model.SimpleModelWrapper;
 import net.minecraft.client.resources.model.geometry.BakedQuad;
 import net.minecraft.client.resources.model.geometry.QuadCollection;
 import net.minecraft.client.resources.model.sprite.Material;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Environment(EnvType.CLIENT)
 @Mixin(SimpleModelWrapper.class)
-public class SimpleModelWrapperMixin implements GlowtoneFullbrightModel {
-
-	@Unique
-	private boolean glowtone$fullbright;
-
-	@Override
-	public boolean glowtone$isFullbright() {
-		return this.glowtone$fullbright;
-	}
+public class SimpleModelWrapperMixin {
 
 	@WrapOperation(
 		method = "bake",
@@ -37,23 +27,21 @@ public class SimpleModelWrapperMixin implements GlowtoneFullbrightModel {
 	private static SimpleModelWrapper glowtone$skipAmbientOcclusionForFullbright(
 		QuadCollection quads, boolean useAmbientOcclusion, Material.Baked particleMaterial, Operation<SimpleModelWrapper> original
 	) {
-		boolean glowtoneShading = GlowtoneConstants.GLOWTONE_SHADING;
-		if (glowtoneShading) {
+		if (useAmbientOcclusion && GlowtoneConstants.GLOWTONE_SHADING) {
 			List<BakedQuad> bakedQuads = quads.getAll();
-			glowtoneShading = !bakedQuads.isEmpty();
-			for (BakedQuad quad : bakedQuads) {
-				if (quad.materialInfo().lightEmission() != 15) {
-					glowtoneShading = false;
-					break;
+			if (!bakedQuads.isEmpty()) {
+				boolean allFullbright = true;
+				for (BakedQuad quad : bakedQuads) {
+					if (quad.materialInfo().lightEmission() != 15) {
+						allFullbright = false;
+						break;
+					}
 				}
+				if (allFullbright)
+					useAmbientOcclusion = false;
 			}
 		}
 
-		if (glowtoneShading)
-			useAmbientOcclusion = false;
-
-		SimpleModelWrapper result = original.call(quads, useAmbientOcclusion, particleMaterial);
-		((SimpleModelWrapperMixin) (Object) result).glowtone$fullbright = glowtoneShading;
-		return result;
+		return original.call(quads, useAmbientOcclusion, particleMaterial);
 	}
 }
